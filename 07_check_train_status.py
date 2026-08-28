39e7ae77144af479b9a00fff61be9da4f90c02fe
rbuler <rafalbuler@gmail.com>
2026-08-28 16:30:14 +0200

    Removed completed TODO

--- 07_check_train_status.py
+++ 07_check_train_status.py
@@ -84,5 +84,3 @@ if missing:
     print("\n--- Brakujące wg ARCHITEKTUR GNN ---")
     print(missing_df.groupby("gnn").size().to_string())
 
-
-# TODO think of the need if we want to analyes 3 seeds
